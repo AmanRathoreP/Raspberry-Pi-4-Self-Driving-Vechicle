d058cbce2e9935f2fe802352f55319b06c01aa33
Aman <aman.proj.rel@gmail.com>
2023-03-30 11:54:36 +0530

    add different optimization options for `C++` build

--- src/master__Raspberry_Pi_4B/src/main.cpp
+++ src/master__Raspberry_Pi_4B/src/main.cpp
@@ -7,7 +7,21 @@ void foo2(); // Declare the foo function
 int main()
 {
     foo();
-    std::cout << "Hello from main" << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    double val = 1L;
+    for (int i = 1; i < 1000000; i++)
+    {
+        for (short unsigned int j = 1; j < 254; j++)
+        {
+            val = val * i * j * i * j * i * j * i * j * i * j;
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+
+    std::cout << "Time taken: " << duration.count() << " seconds\n";
+    std::cout << val * -1 << std::endl;
     foo2();
     return 0;
 }
