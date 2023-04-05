AmanRathoreP/Raspberry-Pi-4-Self-Driving-Vechicle

commit e85c197b25a4c18ad2500db34281462c37ced976

create basic navigation stuff
Problems in the current GUI:
1.) Unable to display the icons of the buttons
2.) Unable to load the data of different stage/scene/panel to the current frame

src/user_display/resources/icons/back.svg
@@ -1,0 +1,2 @@
+<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width="800px" height="800px" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--twemoji" preserveAspectRatio="xMidYMid meet"><path fill="#31373D" d="M34 3H12.475V1.128c0-1.046-.74-1.435-1.645-.865L.69 6.652c-.905.57-.922 1.527-.038 2.127l10.215 6.931c.884.602 1.607.235 1.607-.811V13H34a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2zM.024 26.184c0-.727.5-1.137 1.197-1.137H4.13c1.576 0 2.849 1.061 2.849 2.667c0 1.061-.439 1.772-1.409 2.227v.03c1.288.183 2.303 1.258 2.303 2.576c0 2.137-1.424 3.288-3.516 3.288h-3.12c-.697 0-1.212-.439-1.212-1.151v-8.5zm2.273 3.135h1.182c.742 0 1.227-.439 1.227-1.196c0-.713-.561-1.076-1.227-1.076H2.297v2.272zm0 4.516h1.788c.818 0 1.424-.47 1.424-1.318c0-.712-.545-1.197-1.606-1.197H2.297v2.515zm9.217-7.713c.258-.696.85-1.257 1.621-1.257c.805 0 1.365.53 1.621 1.257l2.971 8.243c.092.242.121.454.121.561c0 .591-.484 1-1.045 1c-.637 0-.955-.333-1.107-.788l-.453-1.424H11.03l-.455 1.409c-.15.47-.469.803-1.09.803c-.607 0-1.122-.454-1.122-1.061c0-.242.076-.424.106-.5l3.045-8.243zm.168 5.501h2.879l-1.41-4.395h-.029l-1.44 4.395zm11.378-6.758c1.106 0 3.258.363 3.258 1.696c0 .546-.379 1.016-.94 1.016c-.621 0-1.046-.53-2.318-.53c-1.879 0-2.849 1.591-2.849 3.439c0 1.803.985 3.349 2.849 3.349c1.272 0 1.788-.637 2.409-.637c.682 0 1 .682 1 1.03c0 1.455-2.288 1.788-3.409 1.788c-3.076 0-5.212-2.439-5.212-5.576c0-3.151 2.121-5.575 5.212-5.575zm4.471 1.212c0-.621.455-1.121 1.137-1.121c.651 0 1.137.424 1.137 1.121v3.273l3.727-3.97c.167-.182.455-.424.879-.424c.576 0 1.121.439 1.121 1.091c0 .393-.242.712-.742 1.212l-2.863 2.818l3.5 3.651c.363.364.637.697.637 1.152c0 .712-.562 1.045-1.183 1.045c-.44 0-.727-.258-1.151-.712l-3.924-4.243v3.864c0 .591-.455 1.091-1.137 1.091c-.651 0-1.137-.424-1.137-1.091v-8.757z"></path></svg>

src/user_display/resources/icons/drawer.svg
@@ -1,0 +1,102 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg 
+   viewBox="0 0 600 600"
+   version="1.1"
+   id="svg9724"
+   sodipodi:docname="menu.svg"
+   inkscape:version="1.2.2 (1:1.2.2+202212051550+b0a8486541)"
+   width="600"
+   height="600"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs9728" />
+  <sodipodi:namedview
+     id="namedview9726"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:showpageshadow="2"
+     inkscape:pageopacity="0.0"
+     inkscape:pagecheckerboard="0"
+     inkscape:deskcolor="#d1d1d1"
+     showgrid="true"
+     inkscape:zoom="0.59480855"
+     inkscape:cx="-49.59579"
+     inkscape:cy="515.29185"
+     inkscape:window-width="1920"
+     inkscape:window-height="1009"
+     inkscape:window-x="0"
+     inkscape:window-y="1080"
+     inkscape:window-maximized="1"
+     inkscape:current-layer="g10449"
+     showguides="true">
+    <inkscape:grid
+       type="xygrid"
+       id="grid9972"
+       originx="0"
+       originy="0" />
+    <sodipodi:guide
+       position="-260,300"
+       orientation="0,-1"
+       id="guide383"
+       inkscape:locked="false" />
+    <sodipodi:guide
+       position="300,520"
+       orientation="1,0"
+       id="guide385"
+       inkscape:locked="false" />
+    <sodipodi:guide
+       position="240,520"
+       orientation="0,-1"
+       id="guide939"
+       inkscape:locked="false" />
+    <sodipodi:guide
+       position="220,80"
+       orientation="0,-1"
+       id="guide941"
+       inkscape:locked="false" />
+  </sodipodi:namedview>
+  
+  <g
+     id="g10449"
+     transform="matrix(0.95173205,0,0,0.95115787,13.901174,12.168794)"
+     style="stroke-width:1.05103">
+    <g
+       id="path10026"
+       inkscape:transform-center-x="-0.59233046"
+       inkscape:transform-center-y="-20.347403"
+       transform="matrix(1.3807551,0,0,1.2700888,273.60014,263.99768)" />
+    <g
+       id="g11314"
+       transform="matrix(1.5092301,0,0,1.3955555,36.774048,-9.4503933)"
+       style="stroke-width:50.6951" />
+    <path
+       style="color:#000000;fill:#000000;stroke-width:1.05104;stroke-linecap:square;stroke-linejoin:round;-inkscape-stroke:none;paint-order:stroke fill markers"
+       d="m 116.82051,533.90848 c 0,-35.77923 -30.001428,-65.78273 -65.71225,-65.78274 -35.710867,10e-6 -65.714445,30.00351 -65.714445,65.78274 0,35.7791 30.003578,65.78265 65.714445,65.78265 35.710822,0 65.71225,-30.00355 65.71225,-65.78265 z"
+       id="path1237" />
+    <path
+       style="color:#000000;fill:#000000;stroke-width:1.05104;stroke-linecap:square;stroke-linejoin:round;-inkscape-stroke:none;paint-order:stroke fill markers"
+       d="m 116.82051,302.6125 c 0,-35.77918 -30.001428,-65.78266 -65.71225,-65.78267 -35.710867,1e-5 -65.714445,30.00349 -65.714445,65.78267 2.6e-5,35.77922 30.003597,65.78043 65.714445,65.78045 35.710806,-2e-5 65.71224,-30.00123 65.71225,-65.78045 z"
+       id="path1235" />
+    <path
+       style="color:#000000;fill:#000000;stroke-width:1.05104;stroke-linecap:square;stroke-linejoin:round;-inkscape-stroke:none;paint-order:stroke fill markers"
+       d="m 116.82051,71.313209 c 0,-35.779199 -30.001428,-65.7804582 -65.71225,-65.7804662 -35.710867,8e-6 -65.714445,30.0012672 -65.714445,65.7804662 0,35.779271 30.003578,65.782741 65.714445,65.782751 35.710822,-1e-5 65.71225,-30.00348 65.71225,-65.782751 z"
+       id="path346" />
+    <path
+       style="color:#000000;fill:#000000;stroke-width:1.05103;stroke-linecap:round;-inkscape-stroke:none"
+       d="m 211.58808,21.527961 a 47.572886,49.785578 0 0 0 -47.57256,49.787552 47.572886,49.785578 0 0 0 47.57256,49.785227 H 568.24859 A 47.572886,49.785578 0 0 0 615.82336,71.315513 47.572886,49.785578 0 0 0 568.24859,21.527961 Z"
+       id="path1011" />
+    <path
+       style="color:#000000;fill:#000000;stroke-width:1.05103;stroke-linecap:round;-inkscape-stroke:none"
+       d="m 211.58808,252.82616 a 47.572886,49.785578 0 0 0 -47.57256,49.78524 47.572886,49.785578 0 0 0 47.57256,49.78523 h 356.66051 a 47.572886,49.785578 0 0 0 47.57477,-49.78523 47.572886,49.785578 0 0 0 -47.57477,-49.78524 z"
+       id="path1011-7" />
+    <path
+       style="color:#000000;fill:#000000;stroke-width:1.05103;stroke-linecap:round;-inkscape-stroke:none"
+       d="m 211.58808,484.1232 a 47.572886,49.785578 0 0 0 -47.57256,49.78523 47.572886,49.785578 0 0 0 47.57256,49.78524 h 356.66051 a 47.572886,49.785578 0 0 0 47.57477,-49.78524 47.572886,49.785578 0 0 0 -47.57477,-49.78523 z"
+       id="path1011-5" />
+  </g>
+</svg>

src/user_display/resources/icons/menu.svg
@@ -1,0 +1,4 @@
+<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M12 4C11.4477 4 11 4.44772 11 5C11 5.55228 11.4477 6 12 6C12.5523 6 13 5.55228 13 5C13 4.44772 12.5523 4 12 4ZM15 5C15 6.65685 13.6569 8 12 8C10.3431 8 9 6.65685 9 5C9 3.34315 10.3431 2 12 2C13.6569 2 15 3.34315 15 5ZM12 11C11.4477 11 11 11.4477 11 12C11 12.5523 11.4477 13 12 13C12.5523 13 13 12.5523 13 12C13 11.4477 12.5523 11 12 11ZM15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12ZM11 19C11 18.4477 11.4477 18 12 18C12.5523 18 13 18.4477 13 19C13 19.5523 12.5523 20 12 20C11.4477 20 11 19.5523 11 19ZM12 22C13.6569 22 15 20.6569 15 19C15 17.3431 13.6569 16 12 16C10.3431 16 9 17.3431 9 19C9 20.6569 10.3431 22 12 22Z" fill="#000000"/>
+</svg>

src/user_display/src/ui/home.qml
@@ -1,19 +1,162 @@
+import QtCore
 import QtQuick
+import QtQuick.Layouts
+import QtQuick.Controls
 
-Window {
+
+ApplicationWindow {
+    id: window
+    width: 360
+    height: 520
     visible: true
-    title: qsTr("Home Scene")
+    title: "User Display for SDV"
 
-    Text {
-        color: "#b3030303"
-        text: qsTr("This is a \nHome Screen!")
-        font.pixelSize: 34
-        horizontalAlignment: Text.AlignHCenter
-        anchors.verticalCenterOffset: 1
-        anchors.horizontalCenterOffset: 0
-        scale: 1
-        font.weight: Font.ExtraLight
-        font.family: "Times New Roman"
-        anchors.centerIn: parent
+
+    Settings {
+        id: settings
+        property string style
+    }
+
+
+    Action {
+        id: navigateBackAction
+        icon.source: stackView.depth > 1 ? "./../../resources/icons/back.svg" : "./../../resources/icons/drawer.svg"
+        onTriggered: {
+            if (stackView.depth > 1) {
+                stackView.pop()
+                listView.currentIndex = -1
+            } else {
+                drawer.open()
+            }
+        }
+    }
+
+
+    Action {
+        id: optionsMenuAction
+        icon.source: "./../../resources/icons/.svg"
+        onTriggered: optionsMenu.open()
+    }
+
+    header: ToolBar {
+        RowLayout {
+            spacing: 20
+            anchors.fill: parent
+
+            ToolButton {
+                action: navigateBackAction
+            }
+
+            Label {
+                id: titleLabel
+                text: listView.currentItem ? listView.currentItem.text : "Home Screen"
+                font.pixelSize: 20
+                elide: Label.ElideRight
+                horizontalAlignment: Qt.AlignHCenter
+                verticalAlignment: Qt.AlignVCenter
+                Layout.fillWidth: true
+            }
+
+            ToolButton {
+                action:optionsMenuAction
+
+                Menu {
+                    id: optionsMenu
+                    x: parent.width - width
+                    transformOrigin: Menu.TopRight
+
+                    Action {
+                        text: "Help"
+                        onTriggered:Qt.openUrlExternally("https://github.com/AmanRathoreP/Raspberry-Pi-4-Self-Driving-Vechicle")
+                    }
+                    Action {
+                        text: "About"
+                        onTriggered: aboutDialog.open()
+                    }
+                }
+            }
+        }
+    }
+
+    Drawer {
+        id: drawer
+        width: Math.min(window.width, window.height) / 3 * 2
+        height: window.height
+        interactive: stackView.depth === 1
+
+        ListView {
+            id: listView
+
+            focus: true
+            currentIndex: -1
+            anchors.fill: parent
+
+            delegate: ItemDelegate {
+                width: listView.width
+                text: model.title
+                highlighted: ListView.isCurrentItem
+                onClicked: {
+                    listView.currentIndex = index
+                    stackView.push(model.source)
+                    drawer.close()
+                }
+            }
+
+            model: ListModel {
+                ListElement { title: "Vehicle's Logs"; source: "qrc:/src/ui/pages/logger.qml" }
+            }
+
+            ScrollIndicator.vertical: ScrollIndicator { }
+        }
+    }
+
+    StackView {
+        id: stackView
+        anchors.fill: parent
+
+        initialItem: Pane {
+            id: pane
+
+            Label {
+                text: "<html><body>Home screen is the place where user can select weather he/she wants<br><ul><li>Semi-Automatic Control</li><li>Automatic Control</li><li>Manual Control</li></ul></body></html>"
+                anchors{
+                    margins: 20
+                    top: logo.bottom
+                    left: parent.left
+                    right: parent.right
+                    bottom: arrow.top
+                }
+                horizontalAlignment: Label.AlignHCenter
+                verticalAlignment: Label.AlignVCenter
+                wrapMode: Label.Wrap
+            }
+
+
+        }
+    }
+
+
+
+    Dialog {
+        id: aboutDialog
+        modal: true
+        focus: true
+        title: "About"
+        x: (window.width - width) / 2
+        y: window.height / 6
+        width: Math.min(window.width, window.height) / 3 * 2
+        contentHeight: aboutColumn.height
+
+        Column {
+            id: aboutColumn
+            spacing: 20
+
+            Label {
+                width: aboutDialog.availableWidth
+                text: "This apps provides freedom to user to interact with SDV in real time with best performance!"
+                wrapMode: Label.Wrap
+                font.pixelSize: 24
+            }
+        }
     }
 }

src/user_display/CMakeLists.txt
@@ -15,7 +15,9 @@ qt_add_executable(appuser_display
 qt_add_qml_module(appuser_display
     URI user_display
     VERSION 1.0
-    QML_FILES src/ui/home.qml
+    QML_FILES
+        "src/ui/home.qml"
+        "src/ui/pages/logger.qml"
 )
 
 set_target_properties(appuser_display PROPERTIES

src/user_display/src/main.cpp
@@ -1,18 +1,17 @@
-
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-
 int main(int argc, char *argv[])
 {
+    QGuiApplication::setApplicationName("User's Display");
+
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/user_display/src/ui/home.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-        &app, []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.load(url);
+
+    engine.load(QUrl(u"qrc:/user_display/src/ui/home.qml"_qs));
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
 }

src/user_display/src/ui/pages/logger.qml
@@ -1,0 +1,9 @@
+import QtQuick
+import QtQuick.Controls
+
+ScrollablePage {
+    Label {
+        text: "Displays the logs of the SDV"
+        anchors.top: parent.top
+    }
+}

commit afedf06135a2d7435acb719a1a6012a87fbe5e06

bug fix of icon image not showing
This bug was introduced in commit e85c197b25a4c18ad2500db34281462c37ced976

src/user_display/resources/icons/back.svg
@@ -1,2 +1,17 @@
-<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-<svg width="800px" height="800px" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--twemoji" preserveAspectRatio="xMidYMid meet"><path fill="#31373D" d="M34 3H12.475V1.128c0-1.046-.74-1.435-1.645-.865L.69 6.652c-.905.57-.922 1.527-.038 2.127l10.215 6.931c.884.602 1.607.235 1.607-.811V13H34a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2zM.024 26.184c0-.727.5-1.137 1.197-1.137H4.13c1.576 0 2.849 1.061 2.849 2.667c0 1.061-.439 1.772-1.409 2.227v.03c1.288.183 2.303 1.258 2.303 2.576c0 2.137-1.424 3.288-3.516 3.288h-3.12c-.697 0-1.212-.439-1.212-1.151v-8.5zm2.273 3.135h1.182c.742 0 1.227-.439 1.227-1.196c0-.713-.561-1.076-1.227-1.076H2.297v2.272zm0 4.516h1.788c.818 0 1.424-.47 1.424-1.318c0-.712-.545-1.197-1.606-1.197H2.297v2.515zm9.217-7.713c.258-.696.85-1.257 1.621-1.257c.805 0 1.365.53 1.621 1.257l2.971 8.243c.092.242.121.454.121.561c0 .591-.484 1-1.045 1c-.637 0-.955-.333-1.107-.788l-.453-1.424H11.03l-.455 1.409c-.15.47-.469.803-1.09.803c-.607 0-1.122-.454-1.122-1.061c0-.242.076-.424.106-.5l3.045-8.243zm.168 5.501h2.879l-1.41-4.395h-.029l-1.44 4.395zm11.378-6.758c1.106 0 3.258.363 3.258 1.696c0 .546-.379 1.016-.94 1.016c-.621 0-1.046-.53-2.318-.53c-1.879 0-2.849 1.591-2.849 3.439c0 1.803.985 3.349 2.849 3.349c1.272 0 1.788-.637 2.409-.637c.682 0 1 .682 1 1.03c0 1.455-2.288 1.788-3.409 1.788c-3.076 0-5.212-2.439-5.212-5.576c0-3.151 2.121-5.575 5.212-5.575zm4.471 1.212c0-.621.455-1.121 1.137-1.121c.651 0 1.137.424 1.137 1.121v3.273l3.727-3.97c.167-.182.455-.424.879-.424c.576 0 1.121.439 1.121 1.091c0 .393-.242.712-.742 1.212l-2.863 2.818l3.5 3.651c.363.364.637.697.637 1.152c0 .712-.562 1.045-1.183 1.045c-.44 0-.727-.258-1.151-.712l-3.924-4.243v3.864c0 .591-.455 1.091-1.137 1.091c-.651 0-1.137-.424-1.137-1.091v-8.757z"></path></svg>
+<?xml version="1.0" encoding="iso-8859-1"?>
+<!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg fill="#000000" height="800px" width="800px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 512.052 512.052" xml:space="preserve">
+<g>
+	<g>
+		<path d="M510.207,414.676c-2.32-23.264-6.805-47.255-14.019-71.608C461.49,226.023,355.26,143.933,234.667,138.804V53.362
+			c0-19.006-22.979-28.524-36.418-15.085l-192,192c-8.331,8.331-8.331,21.839,0,30.17l192,192
+			c13.439,13.439,36.418,3.921,36.418-15.085V327.656c48.213-4.823,103.873,7.119,157.335,48.352
+			c29.572,22.808,56.566,53.666,80.333,93.599C483.421,488.234,512,480.372,512,458.696c0.005-0.959,0.005-0.959,0.025-2.725
+			C512.165,442.689,511.785,430.503,510.207,414.676z M418.059,342.222c-66.551-51.328-136.755-64.332-197.184-55.876
+			c-5.85,0.818-10.162,1.676-12.758,2.331c-9.476,2.39-16.117,10.913-16.117,20.686v76.497L51.503,245.362L192,104.866v55.163
+			c0,12.07,10.005,21.737,22.069,21.321c0.863-0.03,1.551-0.045,3.33-0.076c108.78-2.351,206.919,69.472,237.882,173.917
+			c2.832,9.56,5.193,19.068,7.124,28.488C448.369,367.975,433.564,354.18,418.059,342.222z"/>
+	</g>
+</g>
+</svg>

src/user_display/resources/icons/dot-stack.svg
@@ -1,0 +1,9 @@
+<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11 19H8V16H11V19Z" fill="#1F2328"/>
+<path d="M11 13.5H8V10.5H11V13.5Z" fill="#1F2328"/>
+<path d="M11 8H8V5H11V8Z" fill="#1F2328"/>
+<path d="M16 19H13V16H16V19Z" fill="#1F2328"/>
+<path d="M16 13.5H13V10.5H16V13.5Z" fill="#1F2328"/>
+<path d="M16 8H13V5H16V8Z" fill="#1F2328"/>
+</svg>

src/user_display/resources/icons/drawer.svg
@@ -1,102 +1,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-<svg 
-   viewBox="0 0 600 600"
-   version="1.1"
-   id="svg9724"
-   sodipodi:docname="menu.svg"
-   inkscape:version="1.2.2 (1:1.2.2+202212051550+b0a8486541)"
-   width="600"
-   height="600"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:svg="http://www.w3.org/2000/svg">
-  <defs
-     id="defs9728" />
-  <sodipodi:namedview
-     id="namedview9726"
-     pagecolor="#ffffff"
-     bordercolor="#666666"
-     borderopacity="1.0"
-     inkscape:showpageshadow="2"
-     inkscape:pageopacity="0.0"
-     inkscape:pagecheckerboard="0"
-     inkscape:deskcolor="#d1d1d1"
-     showgrid="true"
-     inkscape:zoom="0.59480855"
-     inkscape:cx="-49.59579"
-     inkscape:cy="515.29185"
-     inkscape:window-width="1920"
-     inkscape:window-height="1009"
-     inkscape:window-x="0"
-     inkscape:window-y="1080"
-     inkscape:window-maximized="1"
-     inkscape:current-layer="g10449"
-     showguides="true">
-    <inkscape:grid
-       type="xygrid"
-       id="grid9972"
-       originx="0"
-       originy="0" />
-    <sodipodi:guide
-       position="-260,300"
-       orientation="0,-1"
-       id="guide383"
-       inkscape:locked="false" />
-    <sodipodi:guide
-       position="300,520"
-       orientation="1,0"
-       id="guide385"
-       inkscape:locked="false" />
-    <sodipodi:guide
-       position="240,520"
-       orientation="0,-1"
-       id="guide939"
-       inkscape:locked="false" />
-    <sodipodi:guide
-       position="220,80"
-       orientation="0,-1"
-       id="guide941"
-       inkscape:locked="false" />
-  </sodipodi:namedview>
-  
-  <g
-     id="g10449"
-     transform="matrix(0.95173205,0,0,0.95115787,13.901174,12.168794)"
-     style="stroke-width:1.05103">
-    <g
-       id="path10026"
-       inkscape:transform-center-x="-0.59233046"
-       inkscape:transform-center-y="-20.347403"
-       transform="matrix(1.3807551,0,0,1.2700888,273.60014,263.99768)" />
-    <g
-       id="g11314"
-       transform="matrix(1.5092301,0,0,1.3955555,36.774048,-9.4503933)"
-       style="stroke-width:50.6951" />
-    <path
-       style="color:#000000;fill:#000000;stroke-width:1.05104;stroke-linecap:square;stroke-linejoin:round;-inkscape-stroke:none;paint-order:stroke fill markers"
-       d="m 116.82051,533.90848 c 0,-35.77923 -30.001428,-65.78273 -65.71225,-65.78274 -35.710867,10e-6 -65.714445,30.00351 -65.714445,65.78274 0,35.7791 30.003578,65.78265 65.714445,65.78265 35.710822,0 65.71225,-30.00355 65.71225,-65.78265 z"
-       id="path1237" />
-    <path
-       style="color:#000000;fill:#000000;stroke-width:1.05104;stroke-linecap:square;stroke-linejoin:round;-inkscape-stroke:none;paint-order:stroke fill markers"
-       d="m 116.82051,302.6125 c 0,-35.77918 -30.001428,-65.78266 -65.71225,-65.78267 -35.710867,1e-5 -65.714445,30.00349 -65.714445,65.78267 2.6e-5,35.77922 30.003597,65.78043 65.714445,65.78045 35.710806,-2e-5 65.71224,-30.00123 65.71225,-65.78045 z"
-       id="path1235" />
-    <path
-       style="color:#000000;fill:#000000;stroke-width:1.05104;stroke-linecap:square;stroke-linejoin:round;-inkscape-stroke:none;paint-order:stroke fill markers"
-       d="m 116.82051,71.313209 c 0,-35.779199 -30.001428,-65.7804582 -65.71225,-65.7804662 -35.710867,8e-6 -65.714445,30.0012672 -65.714445,65.7804662 0,35.779271 30.003578,65.782741 65.714445,65.782751 35.710822,-1e-5 65.71225,-30.00348 65.71225,-65.782751 z"
-       id="path346" />
-    <path
-       style="color:#000000;fill:#000000;stroke-width:1.05103;stroke-linecap:round;-inkscape-stroke:none"
-       d="m 211.58808,21.527961 a 47.572886,49.785578 0 0 0 -47.57256,49.787552 47.572886,49.785578 0 0 0 47.57256,49.785227 H 568.24859 A 47.572886,49.785578 0 0 0 615.82336,71.315513 47.572886,49.785578 0 0 0 568.24859,21.527961 Z"
-       id="path1011" />
-    <path
-       style="color:#000000;fill:#000000;stroke-width:1.05103;stroke-linecap:round;-inkscape-stroke:none"
-       d="m 211.58808,252.82616 a 47.572886,49.785578 0 0 0 -47.57256,49.78524 47.572886,49.785578 0 0 0 47.57256,49.78523 h 356.66051 a 47.572886,49.785578 0 0 0 47.57477,-49.78523 47.572886,49.785578 0 0 0 -47.57477,-49.78524 z"
-       id="path1011-7" />
-    <path
-       style="color:#000000;fill:#000000;stroke-width:1.05103;stroke-linecap:round;-inkscape-stroke:none"
-       d="m 211.58808,484.1232 a 47.572886,49.785578 0 0 0 -47.57256,49.78523 47.572886,49.785578 0 0 0 47.57256,49.78524 h 356.66051 a 47.572886,49.785578 0 0 0 47.57477,-49.78524 47.572886,49.785578 0 0 0 -47.57477,-49.78523 z"
-       id="path1011-5" />
-  </g>
-</svg>

src/user_display/resources/icons/menu.svg
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M12 4C11.4477 4 11 4.44772 11 5C11 5.55228 11.4477 6 12 6C12.5523 6 13 5.55228 13 5C13 4.44772 12.5523 4 12 4ZM15 5C15 6.65685 13.6569 8 12 8C10.3431 8 9 6.65685 9 5C9 3.34315 10.3431 2 12 2C13.6569 2 15 3.34315 15 5ZM12 11C11.4477 11 11 11.4477 11 12C11 12.5523 11.4477 13 12 13C12.5523 13 13 12.5523 13 12C13 11.4477 12.5523 11 12 11ZM15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12ZM11 19C11 18.4477 11.4477 18 12 18C12.5523 18 13 18.4477 13 19C13 19.5523 12.5523 20 12 20C11.4477 20 11 19.5523 11 19ZM12 22C13.6569 22 15 20.6569 15 19C15 17.3431 13.6569 16 12 16C10.3431 16 9 17.3431 9 19C9 20.6569 10.3431 22 12 22Z" fill="#000000"/>
+<svg width="800px" height="800px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none">
+  <path fill="#000000" fill-rule="evenodd" d="M19 4a1 1 0 01-1 1H2a1 1 0 010-2h16a1 1 0 011 1zm0 6a1 1 0 01-1 1H2a1 1 0 110-2h16a1 1 0 011 1zm-1 7a1 1 0 100-2H2a1 1 0 100 2h16z"/>
 </svg>

src/user_display/src/ui/home.qml
@@ -20,7 +20,6 @@ ApplicationWindow {
 
     Action {
         id: navigateBackAction
-        icon.source: stackView.depth > 1 ? "./../../resources/icons/back.svg" : "./../../resources/icons/drawer.svg"
         onTriggered: {
             if (stackView.depth > 1) {
                 stackView.pop()
@@ -34,7 +33,6 @@ ApplicationWindow {
 
     Action {
         id: optionsMenuAction
-        icon.source: "./../../resources/icons/.svg"
         onTriggered: optionsMenu.open()
     }
 
@@ -44,6 +42,7 @@ ApplicationWindow {
             anchors.fill: parent
 
             ToolButton {
+                icon.source: stackView.depth > 1 ? "https://www.svgrepo.com/show/238203/backward.svg" : "https://www.svgrepo.com/show/509382/menu.svg"
                 action: navigateBackAction
             }
 
@@ -58,6 +57,7 @@ ApplicationWindow {
             }
 
             ToolButton {
+                icon.source: "https://www.svgrepo.com/show/452277/dot-stack.svg"
                 action:optionsMenuAction
 
                 Menu {
@@ -121,10 +121,8 @@ ApplicationWindow {
                 text: "<html><body>Home screen is the place where user can select weather he/she wants<br><ul><li>Semi-Automatic Control</li><li>Automatic Control</li><li>Manual Control</li></ul></body></html>"
                 anchors{
                     margins: 20
-                    top: logo.bottom
                     left: parent.left
                     right: parent.right
-                    bottom: arrow.top
                 }
                 horizontalAlignment: Label.AlignHCenter
                 verticalAlignment: Label.AlignVCenter